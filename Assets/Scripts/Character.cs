7609ccfa5ddb8fd29f97e41fffe747d9d76b8f6e
misinterpret <zodiackillerg@gmail.com>
2016-11-02 16:31:18 -0700

    Update from damage-scripting branch

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -4,12 +4,31 @@ using System.Collections;
 public class Character : MonoBehaviour {
 		
 	public int health = 10;
+	public bool damagebreak;
 
+	void Start()
+	{
+		damagebreak = false; 
+	}	
 	void Update() {
 		if (health == 0) {
-			Destroy (gameObject);
-			Debug.Log ("Game Over.....");
+			Time.timeScale = 0;
+			GameObject.Find ("youded").GetComponent<CanvasGroup>().alpha = 1f;
+		}
+		if(damagebreak == true){
+			StartCoroutine ("color", gameObject);
+		}
+		else
+		{
+			GetComponent<SpriteRenderer>().color = Color.white; 
 		}
 	}
-}
 
+	IEnumerator color(GameObject obj){
+		obj.GetComponent<SpriteRenderer> ().color = Color.red;
+		yield return new WaitForSeconds (0.3f);
+		obj.GetComponent<SpriteRenderer> ().color = Color.white;
+		yield return new WaitForSeconds (0.3f);
+		damagebreak = false; 
+	}
+}
