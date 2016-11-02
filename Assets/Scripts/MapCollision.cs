47f420f4f284422602ca668c14a6344cab96a184
clee117 <clee00321@gmail.com>
2016-11-01 19:59:17 -0700

    MapCollision(DONE)
    Works like a charm.

--- Assets/Scripts/MapCollision.cs
+++ Assets/Scripts/MapCollision.cs
@@ -4,22 +4,20 @@ using System.Collections;
 
 public class MapCollision: MonoBehaviour {
 
+	public GameObject[] enemies;
+
 	void OnTriggerEnter2D(Collider2D col) {
 		if(col.gameObject.name == "character") {
-			foreach(GameObject child in gameObject.transform) {
-				if (child.gameObject.tag == "Enemy") {
-					child.gameObject.GetComponent<Enemy>().setActivate (true);
-				}
+			foreach (GameObject enemy in enemies) {
+				enemy.GetComponent<Enemy>().setActivate (true);
 			}
 		}
 	}
 
 	void OnTriggerExit2D(Collider2D col) {
 		if (col.gameObject.name == "character") {
-			foreach (GameObject child in gameObject.transform) {
-				if (child.gameObject.tag == "Enemy") {
-					child.gameObject.GetComponent<Enemy>().setActivate (false);
-				}
+			foreach (GameObject enemy in enemies) {
+				enemy.GetComponent<Enemy>().setActivate (false);
 			}
 		}
 	}
