7030753cd4947bf399ecd52bc25f13759765560f
FatalOz <ozinderbitzin@gmail.com>
2016-11-02 13:54:17 -0700

    Damage indicator
    Player turns red when damage is taken for about half a second

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -4,12 +4,32 @@ using System.Collections;
 public class Character : MonoBehaviour {
 		
 	public int health = 10;
+	public bool damagebreak;
 
+	void Start()
+	{
+		damagebreak = false; 
+	}	
 	void Update() {
 		if (health == 0) {
 			Destroy (gameObject);
 			Debug.Log ("Game Over.....");
 		}
+		if(damagebreak == true){
+			Debug.Log ("taking damage");
+			StartCoroutine ("color");
+		}
+		else
+		{
+			GetComponent<SpriteRenderer>().color = Color.white; 
+		}
+	}
+
+	IEnumerator color(){
+		GetComponent<SpriteRenderer> ().color = Color.red;
+		yield return new WaitForSeconds (0.3f);
+		GetComponent<SpriteRenderer> ().color = Color.white;
+		yield return new WaitForSeconds (0.3f);
+		damagebreak = false; 
 	}
 }
-
--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,30 +1,37 @@
 using UnityEngine;
 using System.Collections;
 
-public class Enemy : MonoBehaviour {
+public class Enemy : MonoBehaviour
+{
 	
 	public int health;
 	private bool activate = false;
 	private bool canAttack = true;
-	public Transform target;//set target from inspector instead of looking in Update
-    public float speed = 30f;
-    public bool getActivate() {
+	public Transform target;
+//set target from inspector instead of looking in Update
+	public float speed = 30f;
+
+	public bool getActivate ()
+	{
 		return activate;	
 	}
-	public void setActivate(bool con) {
+
+	public void setActivate (bool con)
+	{
 		this.activate = con;
 	}
 
 
-	void Start()
-    {
+	void Start ()
+	{
 		target = GameObject.FindGameObjectWithTag ("Player").transform;
-    }
+	}
 
-	void Update() {
+	void Update ()
+	{
 		if (health == 0) {
 			Destroy (gameObject);
-			GameObject[] enemies = gameObject.GetComponentInParent<MapCollision>().enemies;
+			GameObject[] enemies = gameObject.GetComponentInParent<MapCollision> ().enemies;
 			int index = 0;
 			foreach (GameObject enemy in enemies) {
 				if (enemy == gameObject) {
@@ -36,27 +43,30 @@ public class Enemy : MonoBehaviour {
 			}
 		}
 		if (activate && canAttack) {
-            Vector3 direction = target.position - transform.position;
-            direction = direction.normalized;
-            //move towards the player
-            if (Vector3.Distance(transform.position, target.position) > 1f) 
-            {//move if distance from target is greater than 1
-                transform.Translate(direction*speed*Time.deltaTime);
-            }
-        }
-	}
-
-	void OnTriggerEnter2D(Collider2D col) {
-		if (col.gameObject.name == "character") {
-			if (canAttack) {
-				col.gameObject.GetComponent<Character> ().health -= 1;
-				Debug.Log ("Pow!");
-				StartCoroutine (wait ());
+			Vector3 direction = target.position - transform.position;
+			direction = direction.normalized;
+			//move towards the player
+			if (Vector3.Distance (transform.position, target.position) > 1f) {//move if distance from target is greater than 1
+				transform.Translate (direction * speed * Time.deltaTime);
 			}
 		}
 	}
 
-	IEnumerator wait() {
+	void OnTriggerEnter2D (Collider2D col)
+	{
+		if (col.gameObject.name == "character") {
+			Character character = col.gameObject.GetComponent<Character>();
+			if (canAttack) {
+				character.health -= 1;
+				character.damagebreak = true;
+				Debug.Log ("Pow!");
+				StartCoroutine (wait ());
+			} 
+		} 
+	}
+
+	IEnumerator wait ()
+	{
 		canAttack = false;
 		gameObject.GetComponent<BoxCollider2D> ().enabled = false;
 		yield return new WaitForSeconds (1);
