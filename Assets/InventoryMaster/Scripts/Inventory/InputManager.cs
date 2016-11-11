7a3ed339a69a7aebc40c77e71cff5483a6429338
FatalOz <ozinderbitzin@gmail.com>
2016-11-11 14:36:31 -0800

    Inventory System!
    Imported a free online inventory system that works really well. Instead
    of reinventing the wheel (which would have taken at least 5 hours of
    work) we'll use this - it works fantastically. Also makes it very simple
    to add items and such. We can replace the graphic UI of it too.
    Currently, press 'E' to open.

---
+++ Assets/InventoryMaster/Scripts/Inventory/InputManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManager : ScriptableObject
+{
+    public bool UFPS;
+    public KeyCode reloadWeapon = KeyCode.R;
+    public KeyCode throwGrenade = KeyCode.G;
+
+    public KeyCode SplitItem;
+    public KeyCode InventoryKeyCode = KeyCode.E;
+    public KeyCode StorageKeyCode;
+    public KeyCode CharacterSystemKeyCode = KeyCode.E;
+    public KeyCode CraftSystemKeyCode;
+}
